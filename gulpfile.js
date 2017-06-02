52537b6e26474fca24aad7f3a3f545b0cebd29ec
copperkraft <copperkraft@gmail.com>
2017-06-02 17:28:59 +0300

    Added webserver.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 /**
  * Created by Владимир on 23.05.2017.
  */
-var gulp = require('gulp');
-var less = require('gulp-less');
-var concat = require('gulp-concat');
+let gulp = require('gulp');
+let less = require('gulp-less');
+let concat = require('gulp-concat');
+let webserver = require('gulp-webserver');
 
 gulp.task('hello', function() {
     console.log('Hello Zell');
@@ -14,4 +15,15 @@ gulp.task('less', function(){
         .pipe(less()) // Converts LESS to CSS with gulp-less
         .pipe(concat('styles.css'))
         .pipe(gulp.dest('./'))
+});
+
+
+
+gulp.task('webserver', function() {
+    gulp.src('./')
+        .pipe(webserver({
+            livereload: true,
+            open: true,
+            fallback: './index.html'
+        }));
 });
